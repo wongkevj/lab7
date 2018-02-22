62ad175313e5547d6e3cf72178924d0ccd480bba
RandomDentyne <randomdentyne@gmail.com>
2018-02-22 00:29:08 -0800

    slide 37

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,10 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(likeClicked);
+}
+
+function likeClicked(e) {
+	e.preventDefault();
+	ga('send', 'event', 'like', 'click');
 }
